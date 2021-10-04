2fd43b7992589282d1eed97e42363841c73ca290
Mykeeeeee <89806023+Mykeeeeee@users.noreply.github.com>
2021-10-04 16:48:56 -0300

    El maikol
    ya lo iba a terminar pero me falataba tiempo xD

---
+++ src/components/Main.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <section className="main-section">
+        <div className="main-overlay"></div>
+
+        <div className="main-logo">
+          <img
+            src="./assets/logo-cinear-main.svg"
+            alt="logo cinear seccion principal"
+          />
+        </div>
+
+        <div className="main-gradient"></div>
+
+        <video className="main-video" autoPlay muted loop>
+          <source
+            src="https://cdn.videvo.net/videvo_files/video/premium/video0040/large_watermarked/universal_countdown1_preview.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </section>
+      <section className="extern-links-section">
+        <ul>
+          <li>
+           <a href="#"><img src="http://cine.ar/img/tv.cine.ar.svg" alt="meganashe" style={{width: 300, height: 61}} />
+           </a>
+          </li>
+          <li>
+           <a href="#"><img src="http://cine.ar/img/play.cine.ar.svg" alt="nashe" style={{width: 300, height: 61}}/>
+           </a>
+         </li>
+         <li>
+           <a href="#"><img src="http://cine.ar/img/estrenos.cine.ar.svg" alt="ashe" style={{width: 300, height: 61}}/>
+           </a>
+        </li>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
